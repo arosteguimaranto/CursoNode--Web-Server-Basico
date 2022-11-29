30127d76a8d224c2fd01ad582038ee985f248bfb
Arostegui Maranto <arosteguimaranto@outlook.com>
2022-11-29 17:45:23 -0600

    Crear ruta autenticación - Auth - Login1

--- models/server.js
+++ models/server.js
@@ -7,7 +7,11 @@ class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
+
+
         this.usuariosPath = '/api/usuarios';
+        this.authPath = '/api/auth';
+        
 
 
         //conectar base de datos
@@ -38,6 +42,7 @@ class Server{
 
     routes(){
 
+        this.app.use (this.authPath, require('../routes/auth'));
         this.app.use(this.usuariosPath, require('../routes/user'));
        
     }
