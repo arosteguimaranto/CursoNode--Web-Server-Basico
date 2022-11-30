5ef64c6c9c30b16d097b649acc5c977f30980d73
Arostegui Maranto <arosteguimaranto@outlook.com>
2022-11-29 18:17:24 -0600

    Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,53 @@
+const Usuario = require('../models/usuario');
 const { response } = require("express");
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res =  response) => {
+const login = async (req, res = response) => {
+
+    const { correo, password } = req.body;
+
+    try {
+
+        //Verificar si el Email existe
+        const usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            });
+        }
+
+        // Verificar si el usuario esta activo
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - estado: false'
+            })
+        }
+
+        //Verificar la contrase;a
+
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            });
+        }
+        // Generar el JWT
+
+        res.json({
+            msg: 'Login OK',
+    
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            msg: 'Hable con el administrador'
+        })
+    }
 
 
-    res.json({
-        msg: 'Login OK'
-    })
 }
 
 module.exports = {
-    login 
+    login
 }
